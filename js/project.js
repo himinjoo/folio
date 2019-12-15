2a16f685a80cd72318efa957aae02ca0ff95f250
Minjoo Yun <hi@minjoodesign.com>
2019-12-14 18:46:54 -0800

    med edit

--- js/project.js
+++ js/project.js
@@ -58,9 +58,24 @@ $(document).ready(function() {
 	}
 });
 
-let mainNav = document.getElementById("js-menu");
-let navBarToggle = document.getElementById("js-navbar-toggle");
+// let mainNav = document.getElementById("js-menu");
+// let navBarToggle = document.getElementById("js-navbar-toggle");
+//
+// navBarToggle.addEventListener("click", function() {
+//   mainNav.classList.toggle("active");
+// });
 
-navBarToggle.addEventListener("click", function() {
-  mainNav.classList.toggle("active");
-});
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
