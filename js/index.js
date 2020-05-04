3a83cdf2fc0483487ca85a114af327876808ff08
Minjoo Yun <hi@minjoodesign.com>
2020-05-04 13:27:01 -0700

    new banner

--- js/index.js
+++ js/index.js
@@ -16,7 +16,6 @@ $(document).ready(function () {
     });
 });
 
-
 $(document).ready(function() {
 $('html, body, *').mousewheel(function(e, delta) {
 this.scrollLeft -= (delta * 50);
@@ -30,15 +29,18 @@ function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById("nav-wrapper").style.height = "40px";
     document.getElementById("secondNavbar").style.top = "0";
+    document.getElementById("navText").style.opacity = "1";
     document.getElementById("spantitle").style.marginTop = "-21px";
     document.getElementById("logo").style.position = "absolute";
   } else {
     document.getElementById("nav-wrapper").style.height = "179px";
     document.getElementById("secondNavbar").style.top = "-60px";
+    document.getElementById("navText").style.opacity = "0";
     document.getElementById("spantitle").style.marginTop = "0px";
     document.getElementById("logo").style.fontSize = "absolute";
   }
 };
+
 
 $(window).scroll(function() {
   var scroll = $(window).scrollTop();
